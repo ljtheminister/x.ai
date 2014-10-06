65446c848962ae066e17751a58a333da1bf15fbd
ljtheminister <john.c.min@gmail.com>
2014-10-06 00:30:22 -0400

    initial commit - using NLTK library

---
+++ name_gender_id.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from collections import defaultdict
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.parse.dependencygraph import DependencyGraph
+from nltk.stem import porter
+from nltk.tokenize import RegexpTokenizer
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+
+
+from nltk import NaiveBayesClassifier, classify
+
+
+
+class Name_Identifier():
+
+
+
+
+class Gender_Predictor():
+
