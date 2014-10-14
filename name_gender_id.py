1cdd898dfa820eec05dcee7bd4428c602abc82b2
ljtheminister <john.c.min@gmail.com>
2014-10-14 15:50:46 -0400

    finished Name_Identifier and cleaning up Gender_Predictor class, working on various methods

--- name_gender_id.py
+++ name_gender_id.py
@@ -1,3 +1,4 @@
+'''
 import sys
 import os
 from collections import defaultdict
@@ -13,6 +14,8 @@ from nltk.tokenize import word_tokenize
 from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
 from nltk import NaiveBayesClassifier, classify
 from nltk.corpus import names
+'''
+
 import random
 from sklearn.metrics import roc_curve, auc
 from nltk.tag.stanford import NERTagger
@@ -20,29 +23,27 @@ from nltk.tag.stanford import NERTagger
 
 class Name_Identifier():
 
-    def __init__(self, sentence, tagger='Stanford'):
-        self.sentence = sentence
-        self.names = None
+    def __init__(self, tagger='Stanford'):
         self.tagger = tagger
 
-    def get_names(self):
+    def get_names(self, sentence):
         # Use NLTK Tagger
         if self.tagger == 'NLTK':
             tokens = nltk.tokenize.word_tokenize(sentence)
             pos_tags = nltk.pos_tag(tokens)
             ner_tags = nltk.ne_chunk(pos_tags)
-            self.get_names_from_tags(ner_tags, 'NLTK')
 
         # Use Stanford NER Tagger instead of NLTK default
         elif self.tagger == 'Stanford':
             st = NERTagger('/usr/share/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
                        '/usr/share/stanford-ner/stanford-ner.jar') 
-            ner_tags = st.tag(self.sentence.split())
-            self.get_names_from_tags(ner_tags, 'Stanford')
+            ner_tags = st.tag(sentence.split())
 
-    def get_names_from_tags(self, ner_tags, tag_type='Stanford'):
+        return self.get_names_from_tags(ner_tags)
+
+    def get_names_from_tags(self, ner_tags):
         names = []
-        if tag_type == 'Stanford':
+        if self.tagger == 'Stanford':
             name = None
             for i in xrange(len(ner_tags)):
                 ne, ne_tag = ner_tags[i] 
@@ -56,7 +57,7 @@ class Name_Identifier():
                         names.append(name)
                         name = None
 
-        elif tag_type == 'NLTK':
+        elif self.tagger == 'NLTK':
             for subtree in ner_tags.subtrees(filter=lambda t: t.node == 'PERSON'):
                 name = ''
                 for leaf in subtree.leaves():
@@ -65,52 +66,57 @@ class Name_Identifier():
         return names
 
 
-n = Name_Identifier(sentence, 'NLTK')
-n.get_names()
+names = Name_Identifier('NLTK').get_names(sentence)
+names = Name_Identifier('Stanford').get_names(sentence)
+
+GP = Gender_Predictor()
+GP.predict_gender(name)
+
 
 class Gender_Predictor():
     
-    def __init__(self, name):
-        self.name = name
-        self.first_name = name.split()[0]
-        self.__build_classifier()
+    def __init__(self):
+        self.classifier = None
+        self.get_train_and_test_data()
+        self.build_classifier()
 
+    def get_train_and_test_data(self, train_split_pct=.8):
+        names_male = [name for name in nltk.corpus.names.words('male.txt')]
+        names_female = [name for name in nltk.corpus.names.words('female.txt')]
+        names = ([(name, 'male') for name in names.words('male.txt')] +
+                 [(name, 'female') for name in names.words('female.txt')])
 
-    def build_classifier(self):
+        male_set = set(names_male)
+        female_set = set(names_female)
+        male_set -= female_set
+        female_set -= male_set
 
         self.male_set = male_set
         self.female_set = female_set
 
-# Naive Bayes Classifier
-names_male = [name for name in nltk.corpus.names.words('male.txt')]
-names_female = [name for name in nltk.corpus.names.words('female.txt')]
-names = ([(name, 'male') for name in names.words('male.txt')] +
-         [(name, 'female') for name in names.words('female.txt')])
+
+        random.shuffle(names)
+
+        feature_sets = [(name_features(name), gender) for (name, gender) in names]
+        train_set, test_set = feature_sets[1000:], featuresets[:1000]
 
 
-male_set = set(names_male)
-female_set = set(names_female)
-male_set -= female_set
-female_set -= male_set
+    def get_name_features(self):
+        name = self.first_name.lower()
+        return {
+            'last_letter' : name[-1],
+            'last_two' : name[-2:],
+            'last_is_vowel' : (name[-1] in 'aeiouy')
+        }
 
-random.shuffle(names)
-
-feature_sets = [(gender_features(name), gender) for (name, gender) in names]
-train_set, test_set = feature_sets[1000:], featuresets[:1000]
-
-classifier_NB = nltk.NaiveBayesClassifier.train(train_set)
-
-def _nameFeatures(self):
-    name = self.first_name.lower()
-    return {
-        'last_letter' : name[-1],
-        'last_two' : name[-2:],
-        'last_is_vowel' : (name[-1] in 'aeiouy')
-    }
+    def build_classifier(self):
+        # Naive Bayes Classifier
+        self.classifier = nltk.NaiveBayesClassifier.train(train_set)
 
 
-
-
+    def predict_gender(self, name):
+        name_features = self.get_name_features(name) 
+        return self.classifier.classify(name_features)
 
 
 if __name__ == '__main__':
